639df53c8ea4b3e75a0609c7a9725ade8c22e995
Julien Marliac <marliac.julien@gmail.com>
2020-11-24 09:06:29 +0100

    Update version number

--- src/ConsoleApplication.php
+++ src/ConsoleApplication.php
@@ -8,7 +8,7 @@ class ConsoleApplication extends Application
 {
     public function __construct()
     {
-        parent::__construct('PHPUnit Watcher', '1.12.1');
+        parent::__construct('PHPUnit Watcher', '1.23.0');
 
         $this->add(new WatcherCommand());
     }
